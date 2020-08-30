b5cb49178ccb0f834a0d05c493d238c8e8f63530
Florian Pigorsch <fpigorsch@gmail.com>
2020-08-30 14:37:40 +0200

    svgwrite

--- tests/mock_tile_downloader.py
+++ tests/mock_tile_downloader.py
@@ -1,8 +1,4 @@
-import io
 import typing
-
-import cairo
-
 import staticmaps
 
 
@@ -17,10 +13,4 @@ class MockTileDownloader(staticmaps.TileDownloader):
     def get(
         self, provider: staticmaps.TileProvider, cache_dir: str, zoom: int, x: int, y: int
     ) -> typing.Optional[bytes]:
-        if self._dummy_image_data is None:
-            surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, provider.tile_size(), provider.tile_size())
-            file = io.BytesIO()
-            surface.write_to_png(file)
-            file.seek(0)
-            self._dummy_image_data = file.read()
         return self._dummy_image_data
